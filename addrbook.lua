fe4cb7a101edde52b12cc57d44a7d4c4ee0e0036
Evgeniy Vansevich <dev@just4i.ru>
2015-12-30 14:17:00 +0300

    Update addrbook.lua

--- addrbook.lua
+++ addrbook.lua
@@ -2,6 +2,10 @@
 -- addrbook.lua
 --
 
+-- Space 0: User recipients info
+--   Tuple: { user_id (NUM), data (STR) }
+--   Index 0: HASH user_id
+
 -- Space 1: User recipients info
 --   Tuple: { user_id (NUM), rcp_email (STR), rcp_name (NUM), timestamp (NUM), weight (NUM) }
 --   Index 0: TREE { user_id, rcp_email }
@@ -24,40 +28,29 @@ function addrbook_add_recipient(user_id, rcp_email, rcp_name, timestamp)
 	return 0 -- contact weight updated only
 end
 
-function addrbook_load(user_id)
-	user_id = box.unpack('i', user_id)
-	if(uid == nil) then
-		return 0
+function addrbook_load( user_id )
+	if( user_id == nil ) then
+		return nil
 	end
-	local tuple = box.select(0, 0, user_id) -- space 0 index 0
-	if( tuple == nil) then
-		return 0
-	end
+	user_id = box.unpack( 'i', user_id )
+	local tuple = box.select( 0, 0, user_id ) -- space 0 index 0
 	return tuple
 end
 
-function addrbook_save(user_id, book)
-	user_id = box.unpack('i', user_id)
-	if(user_id == nil or book == nil) then
-		return 0
+function addrbook_save( user_id, book )
+	if( user_id == nil or book == nil ) then
+		return nil
 	end
-	local tuple = box.update(0, user_id, "=p", 1, book)
-
-	if( tuple == nil) then
-		return 0
-	end
+	user_id = box.unpack( 'i', user_id )
+	local tuple = box.update( 0, user_id, "=p", 1, book )
 	return tuple
 end
 
-function addrbook_delete(user_id)
-	user_id = box.unpack('i', user_id)
-	if(user_id == nil) then
-		return 0
+function addrbook_delete( user_id )
+	if( user_id == nil ) then
+		return nil
 	end
-	local tuple = box.delete(0, user_id)
-
-	if( tuple == nil) then
-		return 0
-	end
+	user_id = box.unpack( 'i', user_id )
+	local tuple = box.delete( 0, user_id )
 	return tuple
 end
